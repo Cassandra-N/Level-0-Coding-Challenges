ddbb2d2d57340e190c5d93a57df697dfb077779a
Cassandra-N <tianacassandra594@gmail.com>
2021-10-21 21:16:45 +0200

    Task 6 initial commit

---
+++ Task0_6.js
@@ -0,0 +1,19 @@
+function maximum(num1, num2, num3){
+    
+    var max = 0;
+
+    if(num1 > num2){
+        max = num1;
+    }
+    else {
+        max = num2;
+    }
+
+    if(max < num3){
+        max = num3;
+    }
+
+    return max;
+}
+
+console.log(maximum(3,2,4));

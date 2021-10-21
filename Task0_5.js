f114d1ef4b16b8abd5f19b8e637c71bca6de2069
Cassandra-N <tianacassandra594@gmail.com>
2021-10-21 21:15:58 +0200

    Task 5 initial commit

---
+++ Task0_5.js
@@ -0,0 +1,13 @@
+function getAreaOfTriangle(side1, side2, side3){
+
+    var semiPerimeter = 0;
+    var area = 0;
+
+    semiPerimeter = 1/2*(side1 + side2 + side3);
+
+    area = Math.sqrt(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter - side2) * (semiPerimeter-side3));
+
+    return area;
+}
+
+console.log(getAreaOfTriangle(3,4,5));

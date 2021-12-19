83e925f4ba8873a0bb7acf0facb160c12c38d0ce
Cassandra-N <tianacassandra594@gmail.com>
2021-12-19 17:44:12 +0200

    changing global variables

--- Task0_10.js
+++ Task0_10.js
@@ -1,11 +1,11 @@
 function getCommonLetters(string1, string2){
 
-    var string1Array = string1.split("");
-    var string2Array = string2.split("");
+    let string1Array = string1.split("");
+    let string2Array = string2.split("");
 
-    var commonLetters = [];
+    let commonLetters = [];
     
-    for(var i = 0; i < string2Array.length; i++){
+    for(let i = 0; i < string2Array.length; i++){
         if(string1Array.includes(string2Array[i])){
             commonLetters += string2Array[i];
             if(i >= 1 && i < string2Array.length -1){

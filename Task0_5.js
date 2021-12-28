73c5cd3d432f20e69071951553d84586c7f72d1c
Cassandra-N <tianacassandra594@gmail.com>
2021-12-28 14:22:57 +0200

    using prettier

--- Task0_5.js
+++ Task0_5.js
@@ -1,13 +1,17 @@
-function getAreaOfTriangle(side1, side2, side3){
+function getAreaOfTriangle(side1, side2, side3) {
+  let semiPerimeter = 0;
+  let area = 0;
 
-    let semiPerimeter = 0;
-    let area = 0;
+  semiPerimeter = (1 / 2) * (side1 + side2 + side3);
 
-    semiPerimeter = 1/2*(side1 + side2 + side3);
+  area = Math.sqrt(
+    semiPerimeter *
+      (semiPerimeter - side1) *
+      (semiPerimeter - side2) *
+      (semiPerimeter - side3)
+  );
 
-    area = Math.sqrt(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter - side2) * (semiPerimeter-side3));
-
-    return area;
+  return area;
 }
 
-console.log(getAreaOfTriangle(3,4,5));
+console.log(getAreaOfTriangle(3, 4, 5));

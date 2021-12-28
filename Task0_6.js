31903d148a5ae627c8085b952152c2889e0aad69
Cassandra-N <tianacassandra594@gmail.com>
2021-12-28 14:27:00 +0200

    using prettier

--- Task0_6.js
+++ Task0_6.js
@@ -1,19 +1,17 @@
-function maximum(num1, num2, num3){
-    
-    let max = 0;
+function maximum(num1, num2, num3) {
+  let max = 0;
 
-    if(num1 > num2){
-        max = num1;
-    }
-    else {
-        max = num2;
-    }
+  if (num1 > num2) {
+    max = num1;
+  } else {
+    max = num2;
+  }
 
-    if(max < num3){
-        max = num3;
-    }
+  if (max < num3) {
+    max = num3;
+  }
 
-    return max;
+  return max;
 }
 
-console.log(maximum(3,2,4));
+console.log(maximum(3, 2, 4));

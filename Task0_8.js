e879c9ec9ecc24f9e40035b6f35e5eb1458f1d03
Cassandra-N <tianacassandra594@gmail.com>
2022-01-05 16:42:47 +0200

    fixing output

--- Task0_8.js
+++ Task0_8.js
@@ -2,24 +2,25 @@ function getHoursAndMinutes(num) {
   let hours = 0;
   let minutes = 0;
 
-  while (num >= 60) {
-    num -= 60;
+  if (num >= 60) {
+    while (num >= 60) {
+      num -= 60;
+      minutes = num;
+      ++hours;
+    }
+  } else {
     minutes = num;
-    ++hours;
   }
 
-  if (hours == 1 && minutes == 1) {
+  if (hours === 1 && minutes === 1) {
     console.log(hours + " Hour, " + minutes + " minute");
-  }
-  if (hours == 1 && minutes != 1) {
+  } else if (hours === 1 && minutes !== 1) {
     console.log(hours + " Hour, " + minutes + " minutes");
-  }
-  if (hours != 1 && minutes == 1) {
+  } else if (hours !== 1 && minutes === 1) {
     console.log(hours + " Hours, " + minutes + " minute");
-  }
-  if (hours != 1 && minutes != 1) {
+  } else if (hours !== 1 && minutes !== 1) {
     console.log(hours + " Hours, " + num + " minutes");
   }
 }
 
-getHoursAndMinutes(133);
+getHoursAndMinutes(1);
